6d5cbe164f00f1c696d0618d9fc6c343f516299a
Chris <goabonga@pm.me>
2022-09-10 22:49:09 +0200

    chore: add terraform module for example

---
+++ examples/modules/main.tf
@@ -0,0 +1,3 @@
+output "computed" {
+  value = "hello ${var.required}!"
+}
---
+++ examples/modules/variables.tf
@@ -0,0 +1,16 @@
+variable "required" {
+    type = string
+    description = "required value"
+}
+
+variable "optional" {
+    type = string
+    default = "optional"
+    description = "optional value"
+}
+
+variable "nullable" {
+    type = string
+    default = null
+    descrdescription = "nullable value"
+}
