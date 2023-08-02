14a372b00443c4069fc2b09c9a38abe68528e5ad
Chris <goabonga@pm.me>
2023-08-02 23:30:41 +0200

    chore: remove comment and lint

--- generator/utils.py
+++ generator/utils.py
@@ -2,7 +2,7 @@ from os.path import exists
 from urllib.parse import urlparse
 
 
-def is_local(url):
+def is_local(url) -> bool:
     url_parsed = urlparse(url)
     if url_parsed.scheme in ('file', ''):
         return exists(url_parsed.path)
