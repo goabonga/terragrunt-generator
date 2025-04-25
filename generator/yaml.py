82f4ec8afa1481045696fb104006fab48724ab01
Chris <goabonga@pm.me>
2025-04-25 23:45:24 +0200

    fix(yaml): properly format multiline descriptions and default values

--- generator/yaml.py
+++ generator/yaml.py
@@ -2,28 +2,32 @@ import json
 
 
 def get_yaml(name: str, variables, is_enabled: bool = True) -> str:
-    name_parts = name.split('.')
+    def format_description(description: str, indent: str) -> str:
+        lines = description.replace('\\"', '"').split('\n')
+        if not lines:
+            return ''
+        formatted = lines[0]
+        if len(lines) > 1:
+            for line in lines[1:]:
+                formatted += f'\n{indent}# {line.lstrip()}'
+        return formatted
 
+    name_parts = name.split('.')
     indent = '  ' * len(name_parts)
 
     text = ''
     for var_type in ('mandatories', 'optionals', 'nullables'):
         for variable in variables.get(var_type, []):
             default = variable.get('default')
-            description = (
-                variable.get('description', '')
-                .replace('    ', '', 1)
-                .replace('\n', '\n#   #')
-                .replace('\\"', '"')
-            )
+            description = format_description(variable.get('description', ''), indent)
 
             text += f"{indent}# {variable['name']} - {description}\n"
             if var_type == 'nullables':
-                text += f"{indent}# {variable['name']}: {json.JSONEncoder().encode(default) if default else ''}\n"
+                text += f"{indent}# {variable['name']}: {json.dumps(default) if default else ''}\n"
             elif var_type == 'optionals':
-                text += f"{indent}# {variable['name']}: {json.JSONEncoder().encode(default) if default else ''}\n"
+                text += f"{indent}# {variable['name']}: {json.dumps(default) if default else ''}\n"
             else:
-                text += f"{indent}{variable['name']}: {json.JSONEncoder().encode(default) if default else ''}\n"
+                text += f"{indent}{variable['name']}: {json.dumps(default) if default else ''}\n"
 
     nested_yaml = ''
     current_indent = ''
